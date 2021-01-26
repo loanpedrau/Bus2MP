79aa9b3ec31999bc4237ddf54127efb65cd833d5
lpedrau <lpedrau@oceaneconsulting.com>
2021-01-26 10:54:55 +0100

    Initial commit

---
+++ app/src/main/java/fr/istic/mob/bus2mp/MainActivity.java
@@ -0,0 +1,70 @@
+package fr.istic.mob.bus2mp;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Cursor busroutes = getContentResolver().query(Uri.parse("content://fr.istic.mob.busmp.provider/busroute"),
+                null,
+                null,
+                null,
+                null);
+
+        while(busroutes.moveToNext()){
+            int nameIndex = busroutes.getColumnIndex("route_short_name");
+            String name = busroutes.getString(nameIndex);
+            System.out.println("provider - BUS ROUTES :"+name);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
